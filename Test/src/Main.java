971b1279e19377f726992908b8a41adc53814455
Fatihbit2 <123371568+Fatihbit2@users.noreply.github.com>
2023-01-23 12:41:39 +0100

    Update Main.java

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         System.out.println("Hello world!");
                 System.out.println("HIIII!");
                 System.out.println("hallo!");
+                System.out.println("hallo!");
+
 
 
     }
