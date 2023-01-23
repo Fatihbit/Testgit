941ae4ead36735adac610fd1ea4a3e42e5415c0a
Fatihbit2 <123371568+Fatihbit2@users.noreply.github.com>
2023-01-23 13:26:09 +0100

    Update Main.java

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -2,6 +2,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
                 System.out.println("HIIII!");
+<<<<<<< Updated upstream
+=======
+                System.out.println("hallo!");
+                System.out.println("hallo!");
+                System.out.println("hallo4!");
+                System.out.println("halloffff!");
+                System.out.println("dit is extra!");
+
+
+>>>>>>> Stashed changes
 
     }
 }
